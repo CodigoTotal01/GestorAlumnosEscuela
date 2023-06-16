6f2c9014b5024bb6eda7dac44fbf327b7812062f
CodigoTotal01 <palaciostarrillochristian@gmail.com>
2023-06-15 22:12:31 -0500

    20 -Examen

--- src/app/components/examenes/examen-form.component.ts
+++ src/app/components/examenes/examen-form.component.ts
@@ -3,6 +3,9 @@ import {CommonFormComponent} from "../common-form.component";
 import {Examen} from "../../models/examen";
 import {ExamenService} from "../../services/examen.service";
 import {ActivatedRoute, Router} from "@angular/router";
+import {Asignatura} from "../../models/asignatura";
+import {Pregunta} from "../../models/pregunta";
+import Swal from "sweetalert2";
 
 @Component({
     selector: 'app-examen-form',
@@ -10,7 +13,15 @@ import {ActivatedRoute, Router} from "@angular/router";
     styleUrls: ['./examen-form.component.css']
 })
 export class ExamenFormComponent
-    extends CommonFormComponent<Examen, ExamenService> implements OnInit {
+    extends CommonFormComponent<Examen, ExamenService>
+    implements OnInit {
+
+    asignaturasPadre: Asignatura[] = [];
+
+    asignaturasHija: Asignatura[] = [];
+
+
+    errorPreguntas: string;
 
     constructor(service: ExamenService,
                 router: Router,
@@ -22,4 +33,93 @@ export class ExamenFormComponent
         this.redirect = "/examenes";
     }
 
+    override ngOnInit(): void {
+        this.route.paramMap.subscribe(params => {
+            const id: number = +params.get('id'); //imporata mucho el nombre que le hallas dado en la ruta
+            if (id) {
+                this.service.ver(id).subscribe(modelo => {
+                    this.model = modelo;
+                    this.titulo = "Editar " + this.nombreModelo;
+
+                    // //llenar con als asignaturas hijas
+                    // this.service.findAllAsignatura().subscribe(asignatura => {
+                    //
+                    //     this.asignaturasHija = asignatura.filter(asignaturaPadre => {
+                    //
+                    //         return asignaturaPadre.padre && asignaturaPadre.padre.id === this.model.asignaturaPadre.id
+                    //     })
+                    //
+                    // });
+                    this.cargarHijos();
+                })
+            }
+        })
+
+        this.service.findAllAsignatura().subscribe(asignaturas => {
+            //trae padres e hijos
+            this.asignaturasPadre = asignaturas.filter(a => !a.padre);
+        })
+    }
+
+
+    override   crear(): void {
+        if(this.model.preguntas.length === 0 ){
+            Swal.fire("Error Preguntas", "Examen debe tenre preguntas", "error")
+            return;
+        }
+
+        this.eliminarPreguntasVacias();
+        super.crear();
+    }
+
+
+    override editar(): void {
+
+        if(this.model.preguntas.length === 0 ){
+            Swal.fire("Error Preguntas", "Examen debe tenre preguntas", "error")
+            return;
+        }
+        this.eliminarPreguntasVacias();
+
+        super.editar();
+    }
+    cargarHijos() {
+        this.asignaturasHija = this.model.asignaturaPadre ? this.model.asignaturaPadre.hijos : [];
+    }
+
+    compararAsignatura(asignatura1: Asignatura, asignatura2: Asignatura): boolean {
+        if (asignatura1 === undefined && asignatura2 === undefined) {
+            return true;
+        }
+
+        // PAra no mostrar error en consula
+        if (asignatura1 === null || asignatura2 === null || asignatura1 === undefined || asignatura2 === undefined) {
+            return false;
+        }
+
+        //como es asincrono puede uqe los datos no se cargen rapidisimo, ahay un lapto de tiempo que son indefiniddos
+        if (asignatura1.id === asignatura2.id) {
+            return true;
+        }
+
+        return (asignatura1 === null || asignatura2 === null || asignatura1 === undefined || asignatura2 === undefined) ? false : asignatura1.id === asignatura2.id;
+    }
+
+
+    //como es reactivo generara el campo, porque hay un componente de angular que itera sobre las preguntas del examen
+    agregarPregunta() {
+        this.model.preguntas.push(new Pregunta());
+    }
+
+    asignarTexto(pregunta: Pregunta, event: any) {
+        pregunta.texto = event.target.value as string;
+    }
+
+    eliminarPregunta(pregunta: Pregunta) {
+        this.model.preguntas = this.model.preguntas.filter(preguntaRegistrada => pregunta.texto !== preguntaRegistrada.texto);
+    }
+
+    eliminarPreguntasVacias(): void{
+        this.model.preguntas = this.model.preguntas.filter(preguntaRegistrada => preguntaRegistrada.texto != null && preguntaRegistrada.texto.length > 0 );
+    }
 }
--- src/app/services/examen.service.ts
+++ src/app/services/examen.service.ts
@@ -1,15 +1,26 @@
-import { Injectable } from '@angular/core';
+import {Injectable} from '@angular/core';
 import {CommonService} from "./common.service";
 import {Examen} from "../models/examen";
 import {HttpClient} from "@angular/common/http";
 import {BASE_ENDPOINT} from "../config/app";
+import {Observable} from "rxjs";
+import {Asignatura} from "../models/asignatura";
 
 @Injectable({
-  providedIn: 'root'
+    providedIn: 'root'
 })
-export class ExamenService  extends CommonService<Examen>{
-  //sobre escribir el contructor del padre
-  constructor(http: HttpClient) {
-    super(http);
-    this.baseEndPoint = BASE_ENDPOINT + '/examenes';
-  }}
+export class ExamenService extends CommonService<Examen> {
+    //sobre escribir el contructor del padre
+    constructor(http: HttpClient) {
+        super(http);
+        this.baseEndPoint = BASE_ENDPOINT + '/examenes';
+    }
+
+    //obtener lista completa de las asignaturas
+
+    public findAllAsignatura(): Observable<Asignatura[]> {
+        return this.http.get<Asignatura[]>(this.baseEndPoint + "/asignaturas");
+    }
+
+
+}
